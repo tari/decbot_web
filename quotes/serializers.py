a993155b0abcf9662ca98acaa230af4300cc4ab5
Peter Marheine <peter@taricorp.net>
2014-02-28 16:21:39 -0500

    Break lines on pipe in the db.

--- quotes/serializers.py
+++ quotes/serializers.py
@@ -2,7 +2,20 @@ from quotes.models import Quote
 
 from rest_framework import serializers
 
+class LinesField(serializers.WritableField):
+    """
+    Serialize the db `quote` field into individual lines delimited
+    by pipes.
+    """
+    def to_native(self, obj):
+        return obj.split('|')
+
+    def from_native(self, data):
+        return '|'.join(data)
+
 class QuoteSerializer(serializers.HyperlinkedModelSerializer):
+    lines = LinesField(source='quote')
+
     class Meta:
         model = Quote
-        fields = ('id', 'timestamp', 'added_by', 'quote',)
+        fields = ('id', 'timestamp', 'added_by', 'lines')
